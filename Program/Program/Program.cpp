9814f4ac478b4dab031734802e169f982bb135be
Halucyjan <jakubgrzyb1999@gmail.com>
2021-10-16 01:06:42 +0200

    kod dobry jest

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,15 +2,96 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
+struct personalData {
+    std::string forename;
+    std::string surname;
+    int pesel;
+};
+
+struct calendar {
+    int day;
+    int hour[];
+};
+
+class doctor
+{
+public:
+    personalData docData;
+    calendar docCalendar;
+};
+
+class clinic
+{
+public:
+    clinic(std::string clinicName) :name(clinicName) {}
+    std::string name, location;
+    float distance;
+
+    std::vector<doctor> doctors;
+
+    calendar realCalendar;
+
+    void addDoctor(doctor newDoctor) { doctors.push_back(newDoctor); };
+    void showSchedule()
+    {
+        //”nazwa lekarza” \t  “godziny np. 11:00 \t 12:00”
+        //zbigniew stonoga	zofia syfylis \t zygmunt bajgel
+    }
+};
+
+
+
+class interface
+{
+public:
+    void showClinicList()
+    {
+        int sizeOfList = listOfClinics.size();
+        for (int i = 0; i < sizeOfList; i++)
+        {
+            std::cout << "nazwa: " << listOfClinics[i].name << " lokalizacja: " << listOfClinics[i].location << std::endl;
+        };
+
+    };
+    void addClinic(clinic newClinic) { listOfClinics.push_back(newClinic); };
+    void listOfAvalibleDoctors();
+    void searchVisit();
+    void signUpForVisit();
+    void showDeadlines();
+    void locate();
+    void showInterface()
+    {
+        std::cout << "wybierz akcje\n 1. znajdz klinike\n 2. dodaj klinike\n 3. wyswietl liste klinik\n 4. zapisz sie na wizyte";
+    };
+    void loadData();
+    void initialize()
+    {
+        this->loadData();
+        this->showInterface();
+    };
+    float findBestClinic();
+
+private:
+    std::vector<clinic> listOfClinics;
+
+};
+
 
 int main()
 {
-    cout << "Hello World!\n";
-    int dddddd = 69;
-}
+    interface interfejs;
+    clinic klinika1("klinika slonca");
+    clinic klinika2("klinika smierci i rozpaczy");
 
+    //interfejs.initialize();
+    interfejs.addClinic(klinika1);
+    interfejs.addClinic(klinika2);
+    interfejs.showClinicList();
+    return 0;
+}
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
